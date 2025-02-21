0a2063efcf95b4fe7caa2533f5d580a654234652
Yash Dwivedi <yashdubey262@gmail.com>
2025-02-21 17:31:42 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true,
+    allowedHosts: [
+      'vite-project-production-914f.up.railway.app',
+      '*.up.railway.app'  // This will allow all railway.app subdomains
+    ]
+  },
+  server: {
+    host: true,
+    port: process.env.PORT || 3000
+  }
 })
